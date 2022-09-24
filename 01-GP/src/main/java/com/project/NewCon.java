4f21ea95f1315674cad97d0845670f024fc76958
sadnessManager <3634340163@qq.com>
2022-09-24 14:19:36 +0800

    修改:添加NewCon类，修改Manager

---
+++ 01-GP/src/main/java/com/project/NewCon.java
@@ -0,0 +1,8 @@
+package com.project;
+
+public class NewCon {
+    public static void main(String[] args) {
+        System.out.println("这是新加的方法");
+        System.out.println("这是修改的内容");
+    }
+}
--- 02-GP/src/main/java/com/project/Manager.java
+++ 02-GP/src/main/java/com/project/Manager.java
@@ -3,5 +3,6 @@ package com.project;
 public class Manager {
     public static void main(String[] args) {
         System.out.println("这是javaWeb的项目........");
+        System.out.println("新加内容");
     }
 }
