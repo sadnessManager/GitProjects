b0d87c060096b53f21a852fb3f83e45c32a42140
sadnessManager <3634340163@qq.com>
2022-09-24 15:34:41 +0800

    修改:添加NewTest类，修改Manager

--- 02-GP/src/main/java/com/project/Manager.java
+++ 02-GP/src/main/java/com/project/Manager.java
@@ -4,5 +4,6 @@ public class Manager {
     public static void main(String[] args) {
         System.out.println("这是javaWeb的项目........");
         System.out.println("新加内容");
+        System.out.println("新加内容2");
     }
 }
---
+++ 02-GP/src/main/java/com/project/NewTest.java
@@ -0,0 +1,7 @@
+package com.project;
+
+public class NewTest {
+    public static void main(String[] args) {
+        System.out.println("新加的NewTest");
+    }
+}
